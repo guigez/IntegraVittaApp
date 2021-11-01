a48db2c008c905db3e6bf81f624087e2690027f2
Guilherme <70561321+YuutaDragon@users.noreply.github.com>
2021-10-31 22:57:41 -0300

    adicionando a page para esqueceu senha

--- integraVittaApp/src/app/app-routing.module.ts
+++ integraVittaApp/src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
+  { 
+    path: '',
+   loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+  },
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule),
@@ -22,7 +26,12 @@ const routes: Routes = [
     path: 'exercicio-selecionado',
     loadChildren: () => import('./exercicio-selecionado/exercicio-selecionado.module').then( m => m.ExercicioSelecionadoPageModule),
     canActivate: [AuthGuard]
+  },
+  {
+    path: 'esqueceu',
+    loadChildren: () => import('./esqueceu/esqueceu.module').then( m => m.EsqueceuPageModule)
   }
+
 
 
 ];
---
+++ integraVittaApp/src/app/esqueceu/esqueceu.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-esqueceu',
+  templateUrl: './esqueceu.page.html',
+  styleUrls: ['./esqueceu.page.scss'],
+})
+export class EsqueceuPage implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  sendEmail(){}
+}
